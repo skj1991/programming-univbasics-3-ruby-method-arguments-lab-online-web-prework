b20e56e8d54f2509bf48837bc41e3263e2323b45
Stacy Johns <stacykjohns@icloud.com>
2019-07-09 21:37:29 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -13,13 +13,12 @@ name = "Edwin"
 language = "Ember.js"
 introduction_with_language(name, language)
 
-def introduction_with_language_optional(name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+def introduction_with_language_optional(name)
+  puts "Hi, my name is #{name} and I am learning to program in Ruby."
 end
 
 name = "Edwin"
-language = Ruby
-introduction_with_language_optional(name, language)
+introduction_with_language_optional(name)
 
 def introduction_with_language_optional(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
